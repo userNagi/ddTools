3658d3becfd2bbfde0213da9d7ab2accdb10edc9
nagi <nagi@fmqwd.ink>
2023-12-27 09:45:31 +0800

    merge master and push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,14 +11,23 @@ android {
     defaultConfig {
         applicationId = "com.nagi.ddtools"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -35,10 +44,9 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 
---
+++ app/src/main/java/com/nagi/ddtools/Application.kt
@@ -0,0 +1,10 @@
+package com.nagi.ddtools
+
+import android.app.Application
+
+class DdTools : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // TODO: 进行App初始化操作
+    }
+}
